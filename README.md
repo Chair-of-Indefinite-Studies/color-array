a189c425bdbc1ee7d59435f18ca53191a9e936a4
dvberkel <daan.v.berkel.1980@gmail.com>
2015-01-11 16:05:04 +0100

    initialize project
    we used browser-project

---
+++ README.md
@@ -0,0 +1,4 @@
+cois-color-array
+===========
+
+transform a color string into an color array usable by canvas image data
---
+++ color-array.js
@@ -0,0 +1,3 @@
+;(function(color, undefined){
+    /* transform a color string into an color array usable by canvas image data */
+})(window.color = window.color || {})
